35a27b836a88b8b7337f5a57dcf88278175485bd
Manan Jain <manan.nitdl@gmail.com>
2021-12-09 13:12:47 +0530

    701. Insert into a Binary Search Tree

---
+++ Binary Tree/Leetcode/Medium/insertIntoBST.cpp
@@ -0,0 +1,31 @@
+//701. Insert into a Binary Search Tree
+//Leetcode Medium BST
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* insertIntoBST(TreeNode* root, int val) {
+        if(root==NULL){
+            TreeNode*temp = new TreeNode(val);
+            return temp;
+        }
+        if(root->val < val){
+            root->right = insertIntoBST(root->right,val);
+            //will move in the right
+        }
+        if(root->val > val){
+            root->left = insertIntoBST(root->left,val);
+            //will move in the left
+        }
+        return root;
+    }
+};
